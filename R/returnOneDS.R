cfd19d513ab8db30132ff8bb3556d24ab66cf836
Ayishetu Haruna <ayha@vilua.net>
2021-03-18 19:37:26 +0100

    First commit

---
+++ R/returnOneDS.R
@@ -0,0 +1,20 @@
+#'
+#' @title Returns 1
+#' @description This function returns the number one.
+#' @details This function simply returns the integer value 1
+#' @return 1
+#' @author Ayisha
+#' @export
+#'
+returnOneDS <- function(){
+
+  # returns 1: leaving the function abs to demonstrate ability to use
+  # native R functions
+  out <- abs(1)
+
+  # assign the outcome to the data servers
+  return(out)
+
+}
+# AGGREGATE FUNCTION
+# returnOneDS
